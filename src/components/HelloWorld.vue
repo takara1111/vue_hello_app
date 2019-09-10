b0d4a6bd2f8f5b6787ad3c52efba6949a1b31358
takara1356 <goblin7283@gmail.com>
2019-09-10 23:44:01 +0900

    use event

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,37 +2,52 @@
   <div class="hello">
     <h1>{{ title }}</h1>
     <p>{{ message }}</p>
+    <hr />
+    <div>
+      <input type="text" v-model="input" />
+      <button v-on:click="doAction">くりっく</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    title: String,
-    message: String,
+    title: String
+  },
+  data: function() {
+    return {
+      message: "お名前は",
+      input: "no name"
+    };
+  },
+  methods: {
+    doAction: function() {
+      this.message = 'ちわーーーーーーす、' + this.input + 'ぱいせん';
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  div {
-    margin:0px;
-    padding:0px;
-    text-align:left;
-  }
-  h1 {
-    font-size:72pt;
-    font-weight:bold;
-    text-align:right;
-    letter-spacing:-8px;
-    color:#f0f0f0;
-    margin:0px;
-  }
-  p {
-    margin:0px;
-    color:#666;
-    font-size:16pt;
-  }
+div {
+  margin: 0px;
+  padding: 0px;
+  text-align: left;
+}
+h1 {
+  font-size: 72pt;
+  font-weight: bold;
+  text-align: right;
+  letter-spacing: -8px;
+  color: #f0f0f0;
+  margin: 0px;
+}
+p {
+  margin: 0px;
+  color: #666;
+  font-size: 16pt;
+}
 </style>
