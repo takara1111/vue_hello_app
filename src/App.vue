e838ce1a7af461e312ff916024cc41ee612e8a61
takara1356 <goblin7283@gmail.com>
2019-09-12 22:34:31 +0900

    calc

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
   <div id="app">
-    <HelloWorld v-bind:title="message" v-on:result-event="appAction" />
+    <Calc v-bind:title="message" v-on:result-event="appAction" />
     <hr>
-    <p>{{result}}</p>
+    <div><table v-html="log"></table></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc.vue'
 
-// コンポーネントを出力？
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Calc
   },
   data: function () {
     return {
-      message: 'HELLOOOO',
-      result: 'no event',
+      message: 'CALC',
+      result: [],
     };
   },
+  computed: {
+    log:function(){
+      var table = '<tr><th class="head">Expression</th> <th class="head"Value></th></tr>';
+      for (var i in this.result){
+        table += '<tr><td>' + this.result[i][0] + '</td><th>'
+        + this.result[i][1] + '</th></tr>';
+      }
+      return table;
+    }
+  },
+  created: function () {
+    var items = localStorage.getItem('log');
+    var logs = JSON.parse(items);
+    if (logs != null) { this.result = logs; }
+  },
   methods: {
-    appAction: function(message) {
-      this.result = '(*** you send:"' + message +'" ***)';
+    appAction: function(exp, res) {
+      this.result.unshift([exp, res]);
+        if (this.result.length > 10) {
+          this.result.pop();
+        }
+      var log = JSON.stringfy(this.result);
+      localStorage.setItem('log', log);
     }
   }
 }
@@ -34,8 +53,16 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 5px;
+}
+tr td {
+  padding: 5px;
+  border: 1px solid gray;
+}
+tr th.head {
+  background-color: black;
+  color:white; 
 }
 </style>
